bc6871bf9dd82f8f4b90279ab89e41cc7ded64f8
amr <amrhisham99@gmail.com>
2021-02-18 15:19:08 +0200

    second commit

---
+++ public/social-client/build/precache-manifest.5c608cb73889e8b585fde6661a2aa58f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6b835ba063dbe7ca1b90948449a023e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7399b37959bf95210cde",
+    "url": "/static/css/main.74be825f.chunk.css"
+  },
+  {
+    "revision": "c272426880bdde12d1ba",
+    "url": "/static/js/2.9ad93193.chunk.js"
+  },
+  {
+    "revision": "cabe693c8e9800f69e4e45289adc65b4",
+    "url": "/static/js/2.9ad93193.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7399b37959bf95210cde",
+    "url": "/static/js/main.d0ce1a5b.chunk.js"
+  },
+  {
+    "revision": "ec73c6d00432834457ad",
+    "url": "/static/js/runtime-main.0cb2fc20.js"
+  },
+  {
+    "revision": "ab5a57f4c97b6715de37ef2529897d3c",
+    "url": "/static/media/logo.ab5a57f4.PNG"
+  }
+]);
